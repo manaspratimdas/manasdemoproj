f319303a574a6c4a5a5a759f81ad5f987f8f8df6
manaspratimdas <manaspratimdas2004@gmail.com>
2015-09-28 18:19:12 +0530

    Update shtest.sh

--- shtest.sh
+++ shtest.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 TODAY=$(date)
 HOST=$(hostname)
 echo "-----------------------------------------------------"
 echo "Date: $TODAY                     Host:$HOST"
 echo "-----------------------------------------------------"
 # add rest code...
+python pytest.py
